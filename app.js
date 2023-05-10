71b42de38646e2393b532226980aa7b0b8775d94
EtiHaltovsky <et3252760@gmail.com>
2023-05-10 22:57:22 +0300

    change port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const distance = require("./distance")
 
 const cors = require('cors');
 const corsOptions ={
-    origin:'http://localhost:3001', 
+    origin:'http://localhost:8000', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
@@ -23,9 +23,9 @@ const options = {
     rejectUnauthorized: false // Disable SSL/TLS certificate verification
   };
   
-  const req = https.request(options, res => {
-    // ...
-  });
+//   const req = https.request(options, res => {
+//     // ...
+//   });
 //app.use(express.urlencoded({ extended: true }));
 
 
@@ -42,4 +42,4 @@ const options = {
 
 app.use('/ravkav', ravkavRouter)
 
-app.listen(3001);
+app.listen(8000);
